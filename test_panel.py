60be1b9d1ec6b7a650e5f79e28811c28692601f7
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2025-03-14 20:44:13 +0200

    Regex Lab

--- test_panel.py
+++ test_panel.py
@@ -1,3 +1,10 @@
-command = input()
+import re
 
-name, surname, famili = comand
+metin = "Benim e-posta adresim: ornek@example.com"
+desen = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"  # E-posta deseni
+
+eslesme = re.search(desen, metin)
+if eslesme:
+    print("E-posta bulundu:", eslesme.group())
+else:
+    print("E-posta bulunamadı.")
