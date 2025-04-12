836a53051beccab29cea1a3c558883618cda82fb
Ferdi Mehmed Akif <76132036+ferdi7o@users.noreply.github.com>
2025-04-12 15:48:46 +0300

    last question answer!

--- Exam/Final_exam/01. Activation Keys.py
+++ Exam/Final_exam/01. Activation Keys.py
@@ -8,8 +8,35 @@ while True:
     fonction = parts[0]
 
     if fonction == "Contains":
-        pass
+        substring = parts[1]
+        if substring in activation_key:
+            print(f"{activation_key} contains {substring}")
+        else:
+            print("Substring not found!")
     elif fonction == "Flip":
-        pass
+        up_or_low = parts[1]
+        start_index = int(parts[2])
+        end_index = int(parts[3]) # inclusive end index
+        before = activation_key[:start_index]
+        target = activation_key[start_index:end_index]
+        after = activation_key[end_index:]
+
+        if up_or_low == "Upper":
+            target = target.upper()
+        elif up_or_low == "Lower":
+            target = target.lower()
+
+        activation_key = (before + target + after)
+        print(activation_key)
+
     elif fonction == "Slice":
-        pass
+        start_index = int(parts[1])
+        end_index = int(parts[2])
+        before = activation_key[:start_index]
+        target = activation_key[start_index:end_index]
+        after = activation_key[end_index:]
+
+        activation_key = before + after
+        print(activation_key)
+
+print(f"Your activation key is: {activation_key}")
